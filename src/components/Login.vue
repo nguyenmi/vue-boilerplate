2cf3508ffaf64230f5d67f49b7457ac8217770a8
Nguyen Ngoc Dien <diennn@greenglobal.vn>
2021-02-22 16:12:34 +0700

    initial code

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    msg: String
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style></style>
